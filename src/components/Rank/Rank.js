96a450662e134346af8b66321ec9347569439c77
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-09-28 20:50:12 +0530

    Fixing an issue such that the name is always starting with a capital letter

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -5,7 +5,7 @@ const Rank = ({name, entries}) => {
 	return (
 		<div>
 			<div className='white f3 '>
-				{`${name}, your current entry count is`}
+				{`${name.charAt(0).toUpperCase() + name.slice(1)}, your current image detect count is`}
 			</div>
 			<div className='white f1 '>
 				{entries}
